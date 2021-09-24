c824886caf1d3c13ed3ba744be13bca5040de3e8
Veves <vevespm@gmail.com>
2021-09-24 10:32:06 +0200

    html basic

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+
+import '../stylesheets/main.scss';
+
+function App() {
+  return <div className="App">
+    
+    <h1>Mis clubs</h1>
+    <main>
+        <div class="card">
+            <h2>#0 Book club</h2>
+            <p>Abierto entre semana:sí</p>
+            <p>Abierto el fin de semana: sí</p>
+        </div>
+    </main>
+    </div>;
+}
+
+export default App;
