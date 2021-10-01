8e62b99c58bb7ddf28016bc21642ce2f037d39bb
Veves <vevespm@gmail.com>
2021-10-01 19:31:40 +0200

    create basicstructure

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,46 @@ import '../stylesheets/main.scss';
 
 function App() {
   return <div className="App">
-    
-    <h1>Mis clubs</h1>
+   <header>
+      <h1>Mis clubs</h1>
+      <div class="separator"></div>
+    </header>
     <main>
+      <section>
         <div class="card">
-            <h2>#0 Book club</h2>
-            <p>Abierto entre semana:sí</p>
-            <p>Abierto el fin de semana: sí</p>
+          <div class="title">
+            <h3>#0</h3>
+            <h3>Name</h3>
+          </div>
+          <p>Info 1</p>
+          <p>Info 2</p>
         </div>
+      </section>
+      <section>
+        <h2>Añadir un nuevo club</h2>
+        <div class="separator"></div>
+        <form>
+          <input type="text" />
+          <div>
+            <label for="¿Está abierto entre semana?">
+                <input
+                  id="estresemana"
+                  type="checkbox"
+                  value="flightoption1"
+                  name="flightoption1"
+                />
+                Abierto entre semana
+            </label>
+          </div>
+          <div>
+            <label for="miau">
+                <input id="miau" type="checkbox" value="miau" name="miau" />
+                Abierto los fines de semana
+              </label>
+          </div>
+          <input type="button" value="Guardar"/>
+        </form>
+      </section>
     </main>
     </div>;
 }
