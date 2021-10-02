2b640e322e65a24ee5c9a249857718a71be1dc20
Veves <vevespm@gmail.com>
2021-10-02 14:52:02 +0200

    add function create new clubs

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,98 @@
-
-import '../stylesheets/App.scss';
+import "../stylesheets/App.scss";
 import { useState } from "react";
-import "../styles/App.scss";
+import "../stylesheets/App.scss";
 import initialData from "../data/clubslist.json";
 
 function App() {
-  return <div className="App">
-   <header>
-      <h1>Mis clubs</h1>
-      <div class="separator"></div>
-    </header>
-    <main>
-      <section>
-        <div class="card">
-          <div class="title">
-            <h3>#0</h3>
-            <h3>Name</h3>
-          </div>
-          <p>Info 1</p>
-          <p>Info 2</p>
-        </div>
-      </section>
-      <section>
-        <h2>Añadir un nuevo club</h2>
+  //guarda la información de los clubs
+  const [data, setData] = useState(initialData);
+
+  //estos sirven para añadir un nuevo club
+  const [newName, setNewName] = useState("");
+  const [workDays, setWorkDays] = useState(false);
+  const [weekEnd, setWeekEnd] = useState(false);
+
+  //manejar nuevonombre
+  const handleChangeName = (ev) => {
+    setNewName(ev.currentTarget.value);
+  };
+  //manejar nuevo abreenlaborales
+  const handleWorkDays = (ev) => {
+    setWorkDays(ev.currentTarget.value);
+  };
+  //manejar nuevo abrefindes
+  const handleWeekEnd = (ev) => {
+    setWeekEnd(ev.currentTarget.value);
+  };
+  const handleClick= (ev) =>{
+    ev.preventDefault(); 
+    //añadir club nuevo
+    const newClub = {
+    name: newName,
+    openOnWeekDays: workDays,
+    openOnWeekend: weekEnd,
+  };
+  setData( [...data, newClub] );
+ }
+ 
+  
+
+  //aquí dibujo el html de los clubs
+  const htmlClubList = data.map((eachClub, index) => (
+    <li class="card" key={index}>
+      <div class="title">
+        <h3>#{index}</h3>
+        <h3>{eachClub.name}</h3>
+      </div>
+      <p>Abre entre semana {(eachClub.openOnWeekdays = true ? "Sí" : "No")}</p>
+      <p>Abre entre semana {(eachClub.openOnWeekend = true ? "Sí" : "No")}</p>
+    </li>
+  ));
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Mis clubs</h1>
         <div class="separator"></div>
-        <form>
-          <input type="text" />
-          <div>
-            <label for="¿Está abierto entre semana?">
+      </header>
+      <main>
+        <ul>{htmlClubList}</ul>
+
+        <section>
+          <h2>Añadir un nuevo club</h2>
+          <div class="separator"></div>
+          <form>
+            <input type="text" onChange={handleChangeName} />
+            <div>
+              <label for="¿Está abierto entre semana?">
                 <input
                   id="estresemana"
                   type="checkbox"
-                  value="flightoption1"
+                  onChange={handleWorkDays}
+                  value={workDays}
                   name="flightoption1"
                 />
                 Abierto entre semana
-            </label>
-          </div>
-          <div>
-            <label for="miau">
-                <input id="miau" type="checkbox" value="miau" name="miau" />
+              </label>
+            </div>
+            <div>
+              <label for="finde">
+                <input
+                  id="finde"
+                  type="checkbox"
+                  value="finde"
+                  name="finde"
+                  onChange={handleWeekEnd}
+                />
                 Abierto los fines de semana
               </label>
-          </div>
-          <input type="button" value="Guardar"/>
-        </form>
-      </section>
-    </main>
-    </div>;
+            </div>
+            <input type="button" value="Guardar" onClick={handleClick} />
+          </form>
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default App;
